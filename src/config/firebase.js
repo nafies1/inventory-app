4e8d0f71465c759527affb03b1f2f60ea0c992d6
nafies1 <ghuroba.muslim@gmail.com>
2020-06-22 11:15:57 +0700

    add config for firebase. Still on progress

---
+++ src/config/firebase.js
@@ -0,0 +1,24 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
